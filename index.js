81363dfe25e31a5b837e85053af29d51b07ad508
Yeonathan Aizenberg <afula.aizenberg@gmail.com>
2022-04-03 18:20:00 +0300

    make it better

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://chat-chat-yeoni.netlify.app",
         methods: [ "GET", "POST" ],
     },
 })
